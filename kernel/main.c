52cf3be7fc378ef062cfc9e7e7db259daca0dacd
moonflowersl <suns2001@163.com>
2022-01-27 22:06:38 +0800

    debug失败，重修

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,20 @@
 #include "print.h"
 #include "init.h"
-void main(void) {
-    put_str("I am kernel!\n");
-    init_all();
-    asm volatile("sti");
-    put_int(0x12345);
-    while(1);
+#include "debug.h"
+#include "memory.h"
+
+int main(){
+	put_str("\nI am kernel\n");
+	init_all();
+	
+	//asm volatile("sti");	//开启中断
+	
+	void* addr = get_kernel_pages(5);
+	put_str("\n get_kernel_page start vaddr is:");
+	put_int((uint32_t)addr);
+	put_str("\n");
+
+
+	while(1);
+	return 0;
 }
